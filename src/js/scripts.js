fbd2474a8d4640fdcb165adeba88f1d946efe560
Danielle <daniellexsousa@gmail.com>
2022-02-18 23:29:58 -0300

    ajustes finais

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,20 @@
 // Function to play the game and spin the wheel
-function spinWheel(){
+function spinWheel() {
     document.getElementById('wheel').classList.add("wheel");
 };
 
 // Function to subscribe to posts
 function submit() {
-    document.getElementById('subscribe-form').classList.add("hidden");
     var name = document.getElementById('name').value;
-    var subscribeMsgElement = document.getElementById('subscribe-message');
-    subscribeMsgElement.classList.remove("hidden");
-    subscribeMsgElement.innerHTML = 'Hi ' + name + ', your email has been registered. :)';
+    var email = document.getElementById('email').value;
+    if (name == '' && email == '') {
+        alert('Type a name and email!')
+    } else {
+        document.getElementById('subscribe-form').classList.add("hidden");
+        var subscribeMsgElement = document.getElementById('subscribe-message');
+        subscribeMsgElement.classList.remove("hidden");
+        subscribeMsgElement.innerHTML = 'Hi ' + name + ', your email has been registered. :)';
+    }
 };
+
 
